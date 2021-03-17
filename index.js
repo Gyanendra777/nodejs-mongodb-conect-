9d4572921df8aa72e0a0854cb8ebc4b375776045
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-17 19:27:43 +0530

    data ko view se lena our mongodb ko dena aur mongodb se lena our user ko dena kod h keval index.js me

--- index.js
+++ index.js
@@ -4,29 +4,72 @@
 const express = require('express')
 const app = express()
 const port = 3000
-// getting-started.js
-const mongoose = require('mongoose');
-const bodyparser = require('body-parser')
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const kittySchema = new mongoose.Schema({
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/traniosite', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const TranioSitesSchema = new mongoose.Schema({
   name: String,
   phone: String,
   email: String,
   address: String,
 });
 
-const Kitten = mongoose.model('Kitten', kittySchema);
+const TranioSitesModel = mongoose.model('traniosites', TranioSitesSchema);
 
-app.post('/contact', function (req, res) {
-  var myData = new contact(req.body);
-  myData.save().then(()=>{
-    res.send('this item has been saved to the database')
-  }).catch(()=>{
-      res.status(400).send("iteme was not saved to ths database")
+//jb hm  (res) ko chuj krte  h to data ko hm ya to express se ya bongodb se data ko crome pr vejte h or user ko dikhhate h 
+app.get('/aboutus', function(req, res){
+  res.send("hi i am <b>Raju</b>!!!");
+});
+
+//jb hm (req) ko chuj krte h to data ko hm crome se data ko user ke dvara late h apne express ya bongodb me save kr lete h
+app.get('/aboutus2', function(req, res){
+  var a=req.query.num1;
+  var b=req.query.num2;
+  res.send("hi i am Raju!!!"+req.query.a+" "+req.query.b);
+});
+
+
+//jb hm yh methad chlate h to eska mtlb ki hm data base se data ko late h or user ko dete h agr hmara data base nhe mlta h to use hm ek err so krte h if mathad ke throw 
+app.get('/findname', function(req, res){
+  TranioSitesModel.find(function(err, data){
+    if(err)
+    {
+      res.send("findname with error: ");
+      console.log(err);
+    }
+    else
+      res.send(data);
   })
-  // res.status(200).render('contact.pug')
-})
+});
+
+
+// url me ase data ko veje=========== ?name=acbxyz&phone=546584
+////jb hm yh methad chlate h to eska mtlb ki hm data base se data ko late h or user ko dete h agr hmara data base nhe mlta h to use hm ek err so krte h if mathad ke throw 
+app.get('/addname', function(req, res){
+  var n = req.query.name;
+  var p = req.query.phone;
+  var e = req.query.email;
+  var a = req.query.address;
+
+  var tranioSitesObj = new TranioSitesModel();
+  tranioSitesObj.name=n;
+  tranioSitesObj.phone=p;
+  tranioSitesObj.email=e;
+  tranioSitesObj.address=a;
+
+  tranioSitesObj.save(function(err, data){
+    if(err)
+    {
+      res.send("addname with error: ");
+      console.log(err);
+    }
+    else
+      res.send("your data sucece full"+data);
+  })
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
