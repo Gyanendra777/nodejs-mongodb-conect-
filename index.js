cf3e8f486a850c95f18f945d4773654a06bdb3d9
GYANENDRA DUBEY <rajudubey1000@gmail.com>
2021-03-17 08:18:30 +0530

    21 se 29 tk ke bich me jo v deta ko lana or pass krna

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
+
+//vidio link https://youtu.be/seZhGRhbHY0
+
+const express = require('express')
+const app = express()
+const port = 3000
 // getting-started.js
 const mongoose = require('mongoose');
-
-// yha pr jo v ap localhost/______ ke bad likhate h v mongodb me ak data base bn jata h 
-mongoose.connect('mongodb://localhost/mongodb_data_name', {useNewUrlParser: true, useUnifiedTopology: true});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', function() {
-  // we're connected!
-  console.log("we are connected id/ ")
-});
+const bodyparser = require('body-parser')
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const kittySchema = new mongoose.Schema({
-  name: String
+  name: String,
+  phone: String,
+  email: String,
+  address: String,
 });
 
-kittySchema.methods.speak = function () {
-  const greeting = "my name is " + this.name 
-  // console.log(greeting);
-}
+const Kitten = mongoose.model('Kitten', kittySchema);
 
-const mongodb_data_name = mongoose.model('mongodb_data_name', kittySchema);
+app.post('/contact', function (req, res) {
+  var myData = new contact(req.body);
+  myData.save().then(()=>{
+    res.send('this item has been saved to the database')
+  }).catch(()=>{
+      res.status(400).send("iteme was not saved to ths database")
+  })
+  // res.status(200).render('contact.pug')
+})
 
-const gyanendra = new mongodb_data_name({ name: 'gyanendra dubey' });
-
-
-gyanendra.save(function (err, gyanendra) {
-  if (err) return console.error(err);
-  // gyanendra.speak();
-});
-
-mongodb_data_name.find( {nma:"gyanendra"},function (err, mongodb_data_names) {
-  if (err) return console.error(err);
-  console.log(mongodb_data_names);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
